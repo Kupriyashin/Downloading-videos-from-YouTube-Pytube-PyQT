5006027e79b5fae8895bcb76823d4fcbb3966325
Nikita Kupriyashin <kupriyashin.nick@yandex.ru>
2023-04-23 12:03:55 +0300

    Переустановил пайтон

--- main.py
+++
@@ -1,8 +0,0 @@
-from loguru import logger
-import pytube
-
-"""
-Библиотека для скачивания видео с ютуба PyTube - https://tehnojam.ru/category/development/pytube-skachivaem-youtube-video-s-pomoshju-python.html
-Некоторые основные возможности PyQT - https://python-scripts.com/pyqt5#install-pyqt5-designer
-"""
-
